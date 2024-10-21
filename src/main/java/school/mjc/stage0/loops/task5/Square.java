c84d95e087e27c525f1150ea5a9c7a945858d072
xden78 <venera.opt@gmail.com>
2024-10-21 20:30:23 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
+        char eight = '8';
+        side(sideLength);
+        System.out.println();
 
+        for (int i = 0; i < sideLength - 2; i++) {
+            for (int a = 0; a < sideLength; a++) {
+                if (a == 0 || a == sideLength - 1) System.out.print(eight);
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        side(sideLength);
+    }
+
+    public void side(int sideLength) {
+        char eight = '8';
+        for (int i = 0; i < sideLength; i++) {
+            System.out.print(eight);
+        }
     }
 }
