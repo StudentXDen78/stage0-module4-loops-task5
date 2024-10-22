c8d3c57816077a1b1e93770e142422fea19f5b12
xden78 <venera.opt@gmail.com>
2024-10-22 11:07:16 +0300

    5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
         if (height == 0) {
-            System.out.println();
+            // System.out.println();
             return;
         }
 
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task5;
 public class Square {
     public void printSquareFrom8s(int sideLength){
         if (sideLength == 0) {
-            System.out.println();
+            // System.out.println();
             return;
         }
 
